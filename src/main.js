1cdd2698862214340315d0c6a06b0e89e8ff1233
AdrianGerman <adrian.german1019@gmail.com>
2024-05-13 15:13:44 -0600

    vue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import router from '@/router'
 
 Vue.config.productionTip = false;
 
 new Vue({
-  // eslint-disable-next-line prettier/prettier
+  router,  
   render: (h) => h(App)
 }).$mount("#app");
