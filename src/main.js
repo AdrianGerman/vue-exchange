b1b8138ba55ce72062a368cd0c418d31b9a43019
AdrianGerman <adrian.german1019@gmail.com>
2024-05-13 09:24:37 -0600

    add tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  // eslint-disable-next-line prettier/prettier
+  render: (h) => h(App)
 }).$mount("#app");
