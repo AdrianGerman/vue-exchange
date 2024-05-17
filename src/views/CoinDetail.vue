fd36ffbf3cbb037ff749096ccdca7a9f2029db51
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2024-05-17 10:29:12 -0600

    currency conversions

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -44,17 +44,20 @@
                 </div>
 
                 <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
-                    <button
-                        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Cambiar</button>
+                    <button @click="toggleConverter"
+                        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        {{ fromUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
+                    </button>
 
                     <div class="flex flex-row my-5">
                         <label class="w-full" for="convertValue">
-                            <input id="convertValue" type="number"
+                            <input v-model="convertValue" id="convertValue" type="number"
+                                :placeholder="`Valor en ${fromUsd ? 'USD' : asset.symbol}`"
                                 class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" />
                         </label>
                     </div>
 
-                    <span class="text-xl"></span>
+                    <span class="text-xl">{{ convertResult }} {{ fromUsd ? asset.symbol : 'USD' }}</span>
                 </div>
             </div>
             <line-chart class="my-10" :colors="['orange']" :min="min" :max="max"
@@ -96,11 +99,20 @@ export default {
             isLoading: false,
             asset: {},
             history: [],
-            markets: []
+            markets: [],
+            fromUsd: true,
+            convertValue: null
         }
     },
 
     computed: {
+        convertResult() {
+            if (!this.convertValue) {
+                return 0
+            }
+            const result = this.fromUsd ? this.convertValue / this.asset.priceUsd : this.convertValue * this.asset.priceUsd
+            return result.toFixed(4)
+        },
         min() {
             return Math.min(
                 ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
@@ -131,6 +143,9 @@ export default {
     },
 
     methods: {
+        toggleConverter() {
+            this.fromUsd = !this.fromUsd
+        },
         getWebSite(exchange) {
             this.$set(exchange, "isLoading", true)
             return api.getExchange(exchange.exchangeId).then(res => {
