cc6e86681543e15f8857ce41200e93fe6c65a6b5
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2024-05-17 11:29:33 -0600

    modal finish

--- modal/main.js
+++ modal/main.js
@@ -1,38 +1,36 @@
 Vue.component("modal", {
   props: ["title"],
-
   methods: {
     close() {
       this.$emit("close");
     }
   },
-
   template: `
-      <div class="modal-mask">
-        <div class="modal-wrapper">
-          <div class="modal-container">
+    <div class="modal-mask" :class="{ show: true }">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
             <h3>{{ title }}</h3>
-            <div>
-              <slot name="body"></slot>
-            </div>
-            <footer>
-              <button v-on:click="close">Cerrar</button>
-            </footer>
+          </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
+          <div class="modal-footer">
+            <button v-on:click="close">Cerrar</button>
           </div>
         </div>
-      </div>`
+      </div>
+    </div>`
 });
 
 new Vue({
   el: "#app",
-
   data() {
     return {
       title: "Titulo del modal",
       showModal: false
     };
   },
-
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
