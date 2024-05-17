f260cd112b2471f45a5276a48eeb7ece2b9e51f5
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2024-05-17 11:17:59 -0600

    tracker finish

--- tracker/main.js
+++ tracker/main.js
@@ -1,11 +1,25 @@
-new Vue({
-  el: "#app",
+const { createApp } = Vue;
 
+createApp({
   data() {
-    return {};
+    return {
+      title: "",
+      time: "",
+      courses: []
+    };
   },
-
-  computed: {},
-
-  methods: {}
-});
+  computed: {
+    totalTime() {
+      return this.courses.reduce((total, course) => total + Number(course.time), 0);
+    }
+  },
+  methods: {
+    addCourse() {
+      if (this.title && this.time) {
+        this.courses.push({ title: this.title, time: this.time });
+        this.title = "";
+        this.time = "";
+      }
+    }
+  }
+}).mount("#app");
